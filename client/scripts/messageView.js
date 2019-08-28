e239abb5a95fa91c6052f738b84845ccf569be83
Matthew Goyke <mwgoyke@gmail.com>
2019-08-27 21:02:52 -0700

    [WIP] messages displayed on page

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -2,8 +2,8 @@ var MessageView = {
   render: _.template(`
       <div class="chat">
         <div class="username"><%= username %></div>
-        <div><%= message %></div>
-        <div><%= roomname %></div>
-      </div>
-    `)
+        <div class="user-message"><%= text %></div>
+        <div class="roomname"><%= roomname %></div>
+        </div>
+        `)
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,19 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+
   },
 
-  render: function() {
+  render: function(data) {
+    // iterate through data array
+    for (let i = 0; i < data.results.length; i++) {
+      // call render() , passing in the relevant properties of each object
+      let messageEquals = MessageView.render(
+        {username: data.results[i].username, text: data.results[i].text, roomname: data.results[i].roomname}
+      );
+      // append escaped user input 
+      $('#chats').append(messageEquals);
+    }
   }
 
 };
