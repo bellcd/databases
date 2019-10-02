2b3b552c21baa81aa3891d8d4906922c06bf3696
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-01 23:53:45 -0400

    [wip] Customizes tests

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -10,17 +10,35 @@ describe('Persistent Node Chat Server', function() {
 
   beforeEach(function(done) {
     dbConnection = mysql.createConnection({
-      user: 'student',
-      password: 'student',
+      user: 'root',
       database: 'chat'
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+    var tablename1 = "messages"; // TODO: fill this out
+    var tablename2 = "rooms";
+    var tablename3 = "users";
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
-    dbConnection.query('truncate ' + tablename, done);
+    // dbConnection.query('truncate ' + tablename1, done);
+    // dbConnection.query('truncate ' + tablename2, done);
+    // dbConnection.query('truncate ' + tablename3, done);
+
+    dbConnection.query(`ALTER TABLE ${tablename1} DROP FOREIGN KEY id_rooms, id_users`, done);
+    dbConnection.query(`ALTER TABLE ${tablename1} ADD CONSTRAINT id_rooms FOREIGN KEY`, done);
+
+
+
+    dbConnection.query('DELETE FROM ' + tablename1, done);
+    dbConnection.query(`DBCC CHECKIDENT ('chat.dbo.${tablename1}',RESEED, 0)`, done);
+
+    dbConnection.query('DELETE FROM ' + tablename2, done);
+    dbConnection.query(`DBCC CHECKIDENT ('chat.dbo.${tablename2}',RESEED, 0)`, done);
+
+    dbConnection.query('DELETE FROM ' + tablename3, done);
+    dbConnection.query(`DBCC CHECKIDENT ('chat.dbo.${tablename3}',RESEED, 0)`, done);
+
   });
 
   afterEach(function() {
@@ -55,6 +73,7 @@ describe('Persistent Node Chat Server', function() {
         dbConnection.query(queryString, queryArgs, function(err, results) {
           // Should have one result:
           expect(results.length).to.equal(1);
+          console.log(results);
 
           // TODO: If you don't have a column named text, change this test.
           expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
@@ -67,8 +86,8 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-       var queryString = "";
-       var queryArgs = [];
+    var queryString = "";
+    var queryArgs = [];
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
