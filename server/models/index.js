85cbfafa3ef2cb6ed78ae6d1c4bf155f79272c99
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-06 21:01:19 -0400

    [wip] Adds POST route for messages

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 var db = require('../db');
+var Promise = require('bluebird');
 
 module.exports = {
   messages: {
@@ -12,39 +13,85 @@ module.exports = {
       // console.log('inside models.messages.post, message.roomname: ', message.roomname);
       let text = message.text;
 
-      // getting the relevant ID from the users table for a particular username
-      db.dbConnection.query(`SELECT id FROM users WHERE users.username = '${message.username}'`, (err, results) => {
-        if (err) { throw err; }
-        let userID = results[0].id;
+      // promises ?? for getting user_id & roomname_id ?? ... this seems a little WET ...
+      const userID = db.dbConnection.queryAsync(`SELECT id FROM users WHERE users.username = ?`, [message.username])
+        .then((users) => {
+          // if the user table doesn't have a record for that username, create one
+          if (users.length === 0) {
+            // promise to add the user to the DB
+            return module.exports.users.postAsync(message)
+              .then((data) => {
+                // promise to get that just added user's id
+                return db.dbConnection.queryAsync(`SELECT id FROM users WHERE users.username = ?`, [message.username]);
+              })
+              .then((id) => {
+                return id[0].id;
+              })
+              .catch((err) => err);
+          } else {
+            return users[0].id;
+          }
+        })
+        .catch((err) => callback(err, null));
 
-        // adds a roomname record for this room
-        // should we have logic that checks if the room exists already before adding it??
-        db.dbConnection.query(`INSERT INTO rooms (roomname) VALUES ('${message.roomname}')`, (err, results, fields) => {
-          if (err) { throw err; }
-          // console.log('room insert results: ', results);
+      const roomID = db.dbConnection.queryAsync(`SELECT id FROM rooms WHERE rooms.roomname = ?`, [message.roomname])
+        .then((rooms) => {
+          // if the room table doesn't have a record for that roomname, create one
+          if (rooms.length === 0) {
+            // promise to add the room to the DB
+            return module.exports.rooms.postAsync(message)
+              .then((data) => {
+                // promise to get that just added room's id
+                return db.dbConnection.queryAsync(`SELECT id FROM rooms WHERE rooms.roomname = ?`, [message.roomname]);
+              })
+              .then((id) => {
+                return id[0].id;
+              })
+              .catch((err) => err);
+          } else {
+            return rooms[0].id;
+          }
+        })
+        .catch((err) => callback(err, null));
 
-          // get the ID from the rooms table for a given room (this assumes there are values in the rooms table already)
-          db.dbConnection.query(`SELECT id FROM rooms WHERE rooms.roomname = '${message.roomname}'`, (err, results, fields) => {
-            if (err) { throw err; }
-            // console.log('room select results: ', results);
-            let roomnameID = results[0].id;
-
-            let placeholders = [userID, roomnameID];
-            // insert the message text and room / user IDs into the message table
-            db.dbConnection.query(`INSERT INTO messages (text, id_users, id_rooms) VALUES ("${text}", ?, ?)`, placeholders, (err, results, fields) => {
-              // console.log('userID: ', userID);
-              if (err) { throw err; }
-              // console.log('insert results: ', results);
-              callback();
-            });
-          });
+      Promise.all([userID, roomID])
+        .then((results) => {
+          console.log(`results from promise.all: `, results);
+          callback(); // TODO: move this!
         });
-      });
+
+
+
+      // gets the relevant ID from the users table for a particular username
+      // db.dbConnection.query(`SELECT id FROM users WHERE users.username = '${message.username}'`, (err, results) => {
+      //   if (err) { throw err; }
+      //   // adds a roomname record for this room
+      //   // should we have logic that checks if the room exists already before adding it??
+      //   db.dbConnection.query(`INSERT INTO rooms (roomname) VALUES ('${message.roomname}')`, (err, results, fields) => {
+      //     if (err) { throw err; }
+      //     // console.log('room insert results: ', results);
+
+      //     // get the ID from the rooms table for a given room (this assumes there are values in the rooms table already)
+      //     db.dbConnection.query(`SELECT id FROM rooms WHERE rooms.roomname = '${message.roomname}'`, (err, results, fields) => {
+      //       if (err) { throw err; }
+      //       // console.log('room select results: ', results);
+      //       let roomnameID = results[0].id;
+
+      //       let placeholders = [userID, roomnameID];
+      //       // insert the message text and room / user IDs into the message table
+      //       db.dbConnection.query(`INSERT INTO messages (text, id_users, id_rooms) VALUES ("${text}", ?, ?)`, placeholders, (err, results, fields) => {
+      //         // console.log('userID: ', userID);
+      //         if (err) { throw err; }
+      //         // console.log('insert results: ', results);
+      //         callback();
+      //       });
+      //     });
+      //   });
+      // });
     }
   },
 
   users: {
-    // Ditto as above.
     get: function (callback) {
       db.dbConnection.query('SELECT username FROM users', (err, results) => {
         if (err) {
@@ -57,14 +104,23 @@ module.exports = {
     post: function (user, callback) {
       db.dbConnection.query(`SELECT username FROM users WHERE username = '${user.username}'`, (err, results) => {
         if (err) {
+          console.log('1');
           callback(err, null);
         } else if (results.length === 1) {
+          console.log('2');
           // that username already exists in the user table
           callback(new Error('Username unavailable. Please choose another.'), null);
         } else {
-          db.dbConnection.query(`INSERT INTO users (username) VALUES ('${user.username}')`, (err) => {
-            if (err) { callback(err, null); }
-            callback();
+          console.log('3');
+          // console.log('user.username: ', user.username);
+          db.dbConnection.query(`INSERT INTO users (username) VALUES ('${user.username}')`, (err, results) => {
+            if (err) {
+              callback(err, null);
+            } else {
+              // console.log('here');
+              callback();
+              // callback(results, err)  // TODO: why does the promisified version of this function not work with this setup?? ie, it doesn't invoke the next .then() call on the promise returned by this promisified function ...
+            }
           });
         }
       });
@@ -72,7 +128,6 @@ module.exports = {
   },
 
   rooms: {
-    // Ditto as above.
     get: function (callback) {
       db.dbConnection.query('SELECT roomname FROM rooms', (err, results) => {
         if (err) {
@@ -83,6 +138,7 @@ module.exports = {
       });
     },
     post: function (room, callback) {
+      // console.log('room.roomname: ', room.roomname);
       db.dbConnection.query(`SELECT roomname FROM rooms WHERE roomname = '${room.roomname}'`, (err, results) => {
         if (err) {
           callback(err, null);
@@ -90,7 +146,7 @@ module.exports = {
           // that roomname already exists in the room table
           callback(new Error('Room already exists!'), null);
         } else {
-          db.dbConnection.query(`INSERT INTO rooms (roomname) VALUES ('${room.roomname}')`, (err) => {
+          db.dbConnection.query(`INSERT INTO rooms (roomname) VALUES ('${room.roomname}')`, (err, results) => {
             if (err) { callback(err, null); }
             callback();
           });
@@ -99,3 +155,8 @@ module.exports = {
     }
   }
 };
+
+Promise.promisifyAll(db.dbConnection);
+Promise.promisifyAll(module.exports.messages);
+Promise.promisifyAll(module.exports.users);
+Promise.promisifyAll(module.exports.rooms);
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -41,38 +41,6 @@ describe('Persistent Node Chat Server', function() {
     dbConnection.end();
   });
 
-  xit('Should insert POSTed messages to the DB', function(done) {
-    // Post the user to the chat server.
-    request({
-      method: 'POST',
-      uri: 'http://127.0.0.1:3000/classes/users',
-      json: { username: 'Valjean' }
-    }, function() {
-      // Post a message to the node chat server:
-      request({
-        method: 'POST',
-        uri: 'http://127.0.0.1:3000/classes/messages',
-        json: {
-          username: 'Valjean',
-          text: 'In mercy\'s name, three days is all I need.',
-          roomname: 'Hello'
-        }
-      }, function() {
-        // Now if we look in the database, we should find the posted message there.
-        var queryString = 'SELECT * FROM messages';
-        var queryArgs = [];
-
-        dbConnection.query(queryString, queryArgs, function(err, results) {
-          if (err) { throw err; }
-          // Should have one result:
-          expect(results.length).to.equal(1);
-          expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
-          done();
-        });
-      });
-    });
-  });
-
   describe('users table', function() {
     it('Should add a user to the DB', function(done) {
       request({
@@ -219,33 +187,91 @@ describe('Persistent Node Chat Server', function() {
     });
   });
 
-  // TODO: finish writing test
-  xit('Should GET messages from the DB', function(done) {
-    // POST the user to the chat server
-    request({
-      method: 'POST',
-      uri: 'http://127.0.0.1:3000/classes/users',
-      json: { username: 'Christian' }
-    }, function() {
-      done();
+  describe('messages table', function() {
+    xit('Should insert POSTed messages to the DB', function(done) {
+      // Post the user to the chat server.
+      request({
+        method: 'POST',
+        uri: 'http://127.0.0.1:3000/classes/users',
+        json: { username: 'Valjean' }
+      }, function() {
+        // Post a message to the node chat server:
+        request({
+          method: 'POST',
+          uri: 'http://127.0.0.1:3000/classes/messages',
+          json: {
+            username: 'Valjean',
+            text: 'In mercy\'s name, three days is all I need.',
+            roomname: 'Hello'
+          }
+        }, function() {
+          // Now if we look in the database, we should find the posted message there.
+          var queryString = 'SELECT * FROM messages';
+          var queryArgs = [];
+
+          dbConnection.query(queryString, queryArgs, function(err, results) {
+            if (err) { throw err; }
+            // Should have one result:
+            expect(results.length).to.equal(1);
+            expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
+            done();
+          });
+        });
+      });
     });
-  });
 
-  xit('Should output all messages from the DB', function(done) {
-    // Let's insert a message into the db
-    var queryString = "select messages.text, users.username, rooms.roomname from messages inner join users on messages.id = users.id inner join rooms on messages.id_rooms = rooms.id;";
-    var queryArgs = [];
+    it.only('Changing test ... Should insert POSTed messages to the DB', function(done) {
+      // post a message to the chat server, without having posted a user previously
+      request({
+        method: 'POST',
+        uri: 'http://127.0.0.1:3000/classes/messages',
+        json: {
+          username: 'Valjean',
+          text: 'In mercy\'s name, three days is all I need.',
+          roomname: 'Hello'
+        }
+      }, function() {
+        // // Now if we look in the database, we should find the posted message there.
+        // var queryString = 'SELECT * FROM messages';
+        // var queryArgs = [];
 
-    dbConnection.query(queryString, queryArgs, function(err) {
-      if (err) { throw err; }
-
-      // Now query the Node chat server and see if it returns
-      // the message we just inserted:
-      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
-        // var messageLog = JSON.parse(body);
-        // expect(messageLog[0].text).to.equal('Men like you can never change!');
-        // expect(messageLog[0].roomname).to.equal('main');
+        // dbConnection.query(queryString, queryArgs, function(err, results) {
+        //   if (err) { throw err; }
+        //   // Should have one result:
+        //   expect(results.length).to.equal(1);
+        //   expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
         done();
+      });
+    });
+
+    // TODO: finish writing test
+    xit('Should GET messages from the DB', function(done) {
+      // POST the user to the chat server
+      request({
+        method: 'POST',
+        uri: 'http://127.0.0.1:3000/classes/users',
+        json: { username: 'Christian' }
+      }, function() {
+        done();
+      });
+    });
+
+    xit('Should output all messages from the DB', function(done) {
+      // Let's insert a message into the db
+      var queryString = "select messages.text, users.username, rooms.roomname from messages inner join users on messages.id = users.id inner join rooms on messages.id_rooms = rooms.id;";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err) {
+        if (err) { throw err; }
+
+        // Now query the Node chat server and see if it returns
+        // the message we just inserted:
+        request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+          // var messageLog = JSON.parse(body);
+          // expect(messageLog[0].text).to.equal('Men like you can never change!');
+          // expect(messageLog[0].roomname).to.equal('main');
+          done();
+        });
       });
     });
   });
